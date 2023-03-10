1df07e2ecfa74dd9d90cba0b923f8c58b08ed026
Almi's PC <almimeci@gmail.com>
2023-03-10 16:50:48 -0600

    Added User List to user/create

--- src/main/java/com/cydeo/controllers/UserController.java
+++ src/main/java/com/cydeo/controllers/UserController.java
@@ -2,7 +2,7 @@ package com.cydeo.controllers;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
-import com.cydeo.service.impl.RoleServiceImpl;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final RoleService role;
+    private final UserService user;
 
-    public UserController(RoleService role) {
+    public UserController(RoleService role, UserService user) {
         this.role = role;
-
+        this.user = user;
     }
 
 
@@ -24,6 +25,8 @@ public class UserController {
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("listOfRoles", role.findAll() );
+        model.addAttribute("listOfUsers", user.findAll());
+
         return "user/create";
     }
 
