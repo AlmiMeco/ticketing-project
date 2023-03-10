118f213fbb8f65f8088b620146d08a6f939b8178
Almi's PC <almimeci@gmail.com>
2023-03-10 13:10:35 -0600

    UserServiceImpl created

---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
+    @Override
+    public UserDTO save(UserDTO userDTO) {
+        return super.save(userDTO.getUserName(), userDTO);
+    }
+
+    @Override
+    public UserDTO findById(String s) {
+        return super.findById(s);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String s) {
+        super.deleteById(s);
+    }
+}
