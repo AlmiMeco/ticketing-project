87db35d3f42dae655f721fb95653cfe587d6bf1e
Almi's PC <almimeci@gmail.com>
2023-03-11 14:55:12 -0600

    Added Converter Class & "User Create" function finished

--- src/main/java/com/cydeo/controllers/UserController.java
+++ src/main/java/com/cydeo/controllers/UserController.java
@@ -5,27 +5,44 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    private final RoleService role;
-    private final UserService user;
+    private final RoleService roleService;
+    private final UserService userService;
 
     public UserController(RoleService role, UserService user) {
-        this.role = role;
-        this.user = user;
+        this.roleService = role;
+        this.userService = user;
     }
 
 
-    @RequestMapping("/create")
+    @GetMapping("/create")
     public String create(Model model) {
 
         model.addAttribute("user", new UserDTO());
-        model.addAttribute("listOfRoles", role.findAll() );
-        model.addAttribute("listOfUsers", user.findAll());
+        model.addAttribute("listOfRoles", roleService.findAll() );
+        model.addAttribute("listOfUsers", userService.findAll());
+
+        return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String addNewUser(@ModelAttribute("user") UserDTO newlyCreatedUser, Model model){
+
+        model.addAttribute("user", new UserDTO()); // empty object for form (after user is created)
+        model.addAttribute("listOfRoles", roleService.findAll() );
+
+        userService.save(newlyCreatedUser);
+
+        model.addAttribute("listOfUsers", userService.findAll());
+
 
         return "user/create";
     }
