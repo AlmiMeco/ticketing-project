dcb77a0140b3b16b77333408648806ccc0a131d5
Almi's PC <almimeci@gmail.com>
2023-04-18 22:11:37 -0500

    Spring DATA : DAY 21 Part I
    ( Role -> converted to RoleDTO )

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -2,18 +2,23 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Role;
+import com.cydeo.mapper.RoleMapper;
 import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    public final RoleMapper roleMapper;
 
-    public RoleServiceImpl(RoleRepository repository) {
+    public RoleServiceImpl(RoleRepository repository, RoleMapper roleMapper) {
         this.roleRepository = repository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
@@ -23,8 +28,15 @@ public class RoleServiceImpl implements RoleService {
         // Go to repo && find service (DI -> RoleRepo)
         List<Role> roleList = roleRepository.findAll();
 
+        // Role (entity) needs to be converted to -> RoleDTO (DTO)
+        // the conversion is done via Model-Mapper (external lib)
 
-        return roleList;
+        return roleList.stream()
+//               .map(i -> roleMapper.convertToDto(i)) ->  both .map() impl are the same
+                 .map(roleMapper::convertToDto)
+                 .collect(Collectors.toList());
+
+
     }
 
     @Override
