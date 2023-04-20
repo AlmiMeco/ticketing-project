3e299db42c50e180688325fb67e088d8ffcca89b
Almi's PC <almimeci@gmail.com>
2023-04-20 15:50:50 -0500

    Spring DATA : DAY 23 BEGINS!!!!
    ( PrePersist && PreUpdate methods added to BaseEntity )

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @MappedSuperclass
 
+// Behind the scenes fields (NOT visible to end user)
 public class BaseEntity {
 
     @Id
@@ -22,6 +23,26 @@ public class BaseEntity {
     private LocalDateTime insertDateTime;
     private LocalDateTime lastUpdateDateTime;
 
+//    -> Used to differentiate if User will be displayed in Front-End or not
     private Boolean isDeleted = false;
+
+
+                            /* Used to initiate (hidden) fields */
+
+//    Executed upon User creation
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateUserId = 1L;
+    }
+
+//    Executed upon User update
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
 
 }
