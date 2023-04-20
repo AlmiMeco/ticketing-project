01fcc41452f44d3a32fcd81eb13e6e2cc2b75783
Almi's PC <almimeci@gmail.com>
2023-04-20 16:08:33 -0500

    Spring DATA : DAY 23 BEGINS!!!!
    ( Project Service Created )

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,23 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.entity.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface ProjectService {
+
+    List<ProjectDTO> listAllProjects();
+
+    ProjectDTO getByProjectCode(String code);
+
+    void save(ProjectDTO dto);
+
+    void update(ProjectDTO dto);
+
+    void delete(String projectCode);
+
+
+}
