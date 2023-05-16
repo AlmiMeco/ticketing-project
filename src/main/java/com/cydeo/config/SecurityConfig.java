dc43df9c4a73533deaa90d6273cb53dc85047a88
Almi's PC <almimeci@gmail.com>
2023-05-16 16:25:58 -0500

    Spring Security : DAY 26 Security Filter Chain (allowing ANY)

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,7 +20,7 @@ public class SecurityConfig {
 
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
 
         List<UserDetails> userDetailsList = new ArrayList<>();
 
@@ -31,9 +33,26 @@ public class SecurityConfig {
         );
 
 
-
         return new InMemoryUserDetailsManager(userDetailsList);
 
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeRequests().antMatchers(
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images/**"
+                ).permitAll() //allow any User to enter ^^ given end-points
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic() // style of our login form
+                .and().build();
+    }
+
 
 }
